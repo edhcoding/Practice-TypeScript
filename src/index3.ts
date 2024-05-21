2be4ebd4e29417fa8cff49148f9461de57c9d026
eundonghyeok <mephisto7191@naver.com>
2024-05-21 17:15:55 +0900

    객체타입 연습

---
+++ src/index3.ts
@@ -0,0 +1,23 @@
+// object
+// 객체 리터럴 타입
+let user: {
+  id?: number;
+  name: string;
+} = {
+  id: 1,
+  name: "은동혁",
+};
+
+let dog: {
+  name: string;
+  color: string;
+} = {
+  name: "돌돌이",
+  color: "gray",
+};
+
+let config: {
+  readonly apiKey: string;
+} = {
+  apiKey: "MY API KEY",
+};
