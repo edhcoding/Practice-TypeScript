2b980127102b2aa09ee4cb1c1bb7206b76a3bb1f
eundonghyeok <mephisto7191@naver.com>
2024-05-31 01:39:13 +0900

    타입스크립트 템플릿으로 시작하는 방법

---
+++ src/section11/2타입스크립트 템플릿으로 시작하기.ts
@@ -0,0 +1,8 @@
+/**
+ * 타입스크립트로 리액트 프로젝트를 시작할 때 가장 빠르게 시작할 수 있는 도구 소개
+ * Create React App (CRA) 공식문서를 보면 https://create-react-app.dev/docs/adding-typescript
+ * CRA로 생성하는 리액트 앱에 자동으로 타입스크립트 템플릿으로 시작하게 만들 수 있음
+ * npx create-react-app my-app --template typescript
+ * or
+ * yarn create react-app my-app --template typescript
+ */
